11bef9bd2046e75ff0a085bd1bb9c45766f1cc6e
mosharafkarim <69795428+mosharafkarim@users.noreply.github.com>
2025-08-31 00:01:40 +0530

    Update lazy-code.py

--- memory-efficiency-using-genrator-function/lazy-code.py
+++ memory-efficiency-using-genrator-function/lazy-code.py
@@ -1,1 +1,17 @@
 # Code for memory efficient message/custom field extraction from log file
+import sys
+import json
+
+def read_the_file(filepath):
+
+    with open(filepath, 'r') as file:
+
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            yield json.loads(line)
+
+
+log_by_line = read_the_file("large_logs.txt")
+print(next(log_by_line))
